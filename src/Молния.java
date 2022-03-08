36e541b8ac4d2c501096563ae6b1b0cd9354806e
Mishka06 <moihail2044mail.ru@gmail.com>
2022-03-09 00:57:34 +0300

    Initial commit

---
+++ src/Молния.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Молния {
+
+
+
+
+    public static void main(String[] args) {
+        final double v = 1234.8; //скорость звука
+        double t, s;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите временной промежуток между вспышкой молнии и громом (секунд):");
+        t = scanner.nextDouble();
+        s = t * v;
+        System.out.println("Расстояние до места удара молнии: " + s + " метров");
+    }
+}
